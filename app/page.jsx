6eb7d7ea8117fe095bfbf3c87c9026add3bc16c1
j3cohen <joshua3Cohen@gmail.com>
2024-09-23 11:13:07 -0400

    fresh

--- app/page.jsx
+++
@@ -1,5 +0,0 @@
-import { Page as AppPage } from "@/components/app-page"
-
-export default function HomePage() {
-  return <AppPage />
-}
--- lib/utils.ts
+++
@@ -1,6 +0,0 @@
-import { twMerge } from "tailwind-merge";
-import clsx from "clsx";
-
-export function cn(...inputs: unknown[]) {
-  return twMerge(clsx(inputs));
-}
